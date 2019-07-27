fa6fcbd70c6568085700e00898d06ff544a178b8
happou31 <happo31@gmail.com>
2019-07-28 00:25:34 +0900

    fix: AWS でいい感じになるようにした

--- src/get-amesh.ts
+++ src/get-amesh.ts
@@ -1,44 +1,26 @@
 import fetch from "node-fetch";
-import { writeFile, stat, Stats, mkdir } from "fs";
 import sharp = require("sharp");
 
 const ameshPrefix = "https://tokyo-ame.jwa.or.jp/mesh/000/";
 const mapUrl = "https://tokyo-ame.jwa.or.jp/map/map000.jpg";
 const maskUrl = "https://tokyo-ame.jwa.or.jp/map/msk000.png";
 
-const tmpDir = ".tmp";
-
 export default async function getAmesh() {
   const now = new Date();
   // 時間が5分刻みの直後すぎると画像がサーバーで生成されてないっぽいので最大10分前の画像を取るようにする
   const nowStr = dateStr(new Date(now.getTime() - 5 * 60 * 1000));
   try {
-    const dirExists = await fileExists(tmpDir);
-    if (dirExists == null) {
-      await createDirAsync(tmpDir);
-    }
-
-    const mapExists = await fileExists(`${tmpDir}/map.jpg`);
-    if (mapExists == null) {
-      const map = await fetch(mapUrl);
-      await writeFileAsync(`${tmpDir}/map.jpg`, await map.buffer());
-    }
-
-    const maskExists = await fileExists(`${tmpDir}/mask.png`);
-    if (maskExists == null) {
-      const mask = await fetch(maskUrl);
-      await writeFileAsync(`${tmpDir}/mask.png`, await mask.buffer());
-    }
-
+    const map = await fetch(mapUrl);
+    const mask = await fetch(maskUrl);
     const lain = await fetch(`${ameshPrefix}/${nowStr}.gif`);
 
-    return sharp(`${tmpDir}/map.jpg`)
+    return sharp(await map.buffer())
       .composite([
         {
           input: await lain.buffer()
         },
         {
-          input: `${tmpDir}/mask.png`
+          input: await mask.buffer()
         }
       ])
       .toBuffer();
@@ -57,41 +39,14 @@ function dateStr(date: Date): string {
   );
 }
 
-async function fileExists(path: string) {
-  return new Promise<Stats | null>(res => {
-    stat(path, (err, stats) => {
-      if (err) {
-        res(null);
-        return;
-      }
-      res(stats);
-    });
-  });
-}
-
-async function writeFileAsync(path: string, data: unknown) {
-  return new Promise((res, rej) => {
-    writeFile(path, data, err => {
-      if (err) {
-        rej(err);
-        return;
-      }
-      res();
-    });
-  });
-}
-
-async function createDirAsync(path: string) {
-  return new Promise((res, rej) => {
-    mkdir(path, err => {
-      if (err) {
-        rej(err);
-        return;
-      }
-      res();
-    });
-  });
-}
-
 // for AWS Lambda
-exports.handler = getAmesh;
+exports.handler = async (event: any, context: any, callback: any) => {
+  try {
+    const buffer = await getAmesh();
+    if (buffer) {
+      callback(null, buffer.toString("base64"));
+    }
+  } catch (e) {
+    callback(e, null);
+  }
+};
